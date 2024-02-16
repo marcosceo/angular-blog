3f3f55b33f7ab4480fadfc1e4c268d5fc36d1bbf
root <root@XPS-13>
2024-02-16 20:42:25 -0300

    Finished Tutorial

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "title": "Senhor dos Anéis",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, elementum non luctus vitae, sagittis ut sapien. Etiam fringilla, lectus id imperdiet pellentesque, quam risus ornare est, at luctus lacus justo vel enim. Curabitur efficitur sit amet ligula et auctor. Aenean in faucibus metus. Aenean elit lorem, efficitur eu lectus ac, tincidunt tincidunt leo. Etiam ac lacus non lectus eleifend ultricies. Maecenas pulvinar vulputate ultricies. Aliquam vel felis ultricies, molestie dolor quis, tempor velit. Phasellus ullamcorper augue sed laoreet laoreet. Ut efficitur ligula orci, id blandit diam finibus vitae. Sed at urna interdum, sagittis elit eu, imperdiet ante.",
+    "photoCover": "https://cdn.maioresemelhores.com/imagens/melhores-filmes-de-aventura-og.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Avatar",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, elementum non luctus vitae, sagittis ut sapien. Etiam fringilla, lectus id imperdiet pellentesque, quam risus ornare est, at luctus lacus justo vel enim. Curabitur efficitur sit amet ligula et auctor. Aenean in faucibus metus. Aenean elit lorem, efficitur eu lectus ac, tincidunt tincidunt leo. Etiam ac lacus non lectus eleifend ultricies. Maecenas pulvinar vulputate ultricies. Aliquam vel felis ultricies, molestie dolor quis, tempor velit. Phasellus ullamcorper augue sed laoreet laoreet. Ut efficitur ligula orci, id blandit diam finibus vitae. Sed at urna interdum, sagittis elit eu, imperdiet ante.",
+    "photoCover": "https://th.bing.com/th/id/OIP.8aDAGj9M6jgHIAZiVRDtXgHaE8?rs=1&pid=ImgDetMain"
+  },
+  {
+    "id": 3,
+    "title": "Liga da Justiça",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, elementum non luctus vitae, sagittis ut sapien. Etiam fringilla, lectus id imperdiet pellentesque, quam risus ornare est, at luctus lacus justo vel enim. Curabitur efficitur sit amet ligula et auctor. Aenean in faucibus metus. Aenean elit lorem, efficitur eu lectus ac, tincidunt tincidunt leo. Etiam ac lacus non lectus eleifend ultricies. Maecenas pulvinar vulputate ultricies. Aliquam vel felis ultricies, molestie dolor quis, tempor velit. Phasellus ullamcorper augue sed laoreet laoreet. Ut efficitur ligula orci, id blandit diam finibus vitae. Sed at urna interdum, sagittis elit eu, imperdiet ante.",
+    "photoCover": "https://th.bing.com/th/id/R.95b8b6ce12603f7a7328bb48cceeab7e?rik=g47jrw9Idksk6w&pid=ImgRaw&r=0"
+  },
+  {
+    "id": 4,
+    "title": "Rambo",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, elementum non luctus vitae, sagittis ut sapien. Etiam fringilla, lectus id imperdiet pellentesque, quam risus ornare est, at luctus lacus justo vel enim. Curabitur efficitur sit amet ligula et auctor. Aenean in faucibus metus. Aenean elit lorem, efficitur eu lectus ac, tincidunt tincidunt leo. Etiam ac lacus non lectus eleifend ultricies. Maecenas pulvinar vulputate ultricies. Aliquam vel felis ultricies, molestie dolor quis, tempor velit. Phasellus ullamcorper augue sed laoreet laoreet. Ut efficitur ligula orci, id blandit diam finibus vitae. Sed at urna interdum, sagittis elit eu, imperdiet ante.",
+    "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2019/10/20191018112239.jpg"
+  },
+  {
+    "id": 5,
+    "title": "Duro de Matar",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, elementum non luctus vitae, sagittis ut sapien. Etiam fringilla, lectus id imperdiet pellentesque, quam risus ornare est, at luctus lacus justo vel enim. Curabitur efficitur sit amet ligula et auctor. Aenean in faucibus metus. Aenean elit lorem, efficitur eu lectus ac, tincidunt tincidunt leo. Etiam ac lacus non lectus eleifend ultricies. Maecenas pulvinar vulputate ultricies. Aliquam vel felis ultricies, molestie dolor quis, tempor velit. Phasellus ullamcorper augue sed laoreet laoreet. Ut efficitur ligula orci, id blandit diam finibus vitae. Sed at urna interdum, sagittis elit eu, imperdiet ante.",
+    "photoCover": "https://th.bing.com/th/id/R.44167d609f4ca37eb15e139e08d9ed25?rik=FsuU0b2aXqbQLw&pid=ImgRaw&r=0"
+  },
+]
